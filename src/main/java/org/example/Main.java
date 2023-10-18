24186250620f9956af7be9d8fe4cba19dc2f3561
vijayktech <vijay.techgeek@gmail.com>
2023-10-18 15:51:01 +0530

    updated main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Welcome to Java 8 demos !!!");
+        System.out.printf("Welcome to Java 8 demos -done !!!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
